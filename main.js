a02a27aee897d771618cfbcd64dd3f0391a259e2
saeleuterio <saeleuterio@hotmail.com>
2024-10-22 09:59:04 -0300

    up

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+function alternarSubmenu(item, mostrar) {
+    const submenu = item.querySelector(".submenu");
+
+    if (submenu) {
+        submenu.style.display = mostrar ? "block" : "none";
+    }
+}
 // selecionar todo os cabecalho__lista-item
 document.querySelectorAll(".cabecalho__lista-item").forEach(item => {
     // adicionar um ouvinte mouseover
@@ -5,4 +12,12 @@ document.querySelectorAll(".cabecalho__lista-item").forEach(item => {
 
     // adicionar um ouvinte mouseout
     item.addEventListener("mouseout", () => alternarSubmenu(item, false));
+
+    item.addEventListener("click", () => {
+        const submenu = item.querySelector(".submenu");
+
+        const isDisplayed = submenu.style.display === "block";
+
+        alternarSubmenu(item, !isDisplayed);
+    })
 });
