d9cf400f6a18a1b293758e2c0f9f26384fee394b
saeleuterio <saeleuterio@hotmail.com>
2024-10-22 11:16:27 -0300

    up

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
+function alternarModal(modalId, abrir) {
+    const modal = document.querySelector(`#${modalId}`);
+
+    if (abrir) {
+        modal.style.display = 'block';
+    } else {
+        modal.style.display = 'none';
+    }
+
+    document.body.style.overflow = abrir ? "hidden" : "auto";
+}
+
 function alternarSubmenu(item, mostrar) {
     const submenu = item.querySelector(".submenu");
 
     if (submenu) {
         submenu.style.display = mostrar ? "block" : "none";
+
+        const menuItem = item.querySelector(".cabecalho__lista-item a");
+        menuItem.setAttribute("aria-expanded", mostrar ? true : false);
+        
+        const DropdownExpandedIcon = item.querySelector(".material-symbols-outlined.icone");
+
+        DropdownExpandedIcon.classList.toggle("active", mostrar);
     }
 }
 // selecionar todo os cabecalho__lista-item
